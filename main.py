ac7fe0900dba99c9e0aba87cae9f5d64b024880d
aamiransari072 <amirbxr2003@gmail.com>
2024-01-19 22:46:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# lodding intiall librabry
+from ultralytics import YOLO
+import cv2
+
+# loding model
+model = YOLO('yolov8n.pt')
+
+# loading video
+path = './video.mp4'
+
+# read frmaes
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret,frame = cap.read()
+    if ret==True:
+        # detecting objects from frame
+        # track object
+        result = model.track(frame, persist=True)
+
+        #plot results
+        frame_ = result[0].plot()
+
+        # visulaize
+        cv2.imshow('frame',frame_)
+        if cv2.waitKey(1)==ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
